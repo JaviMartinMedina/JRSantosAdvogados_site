f5b45ef3a0e694ec9fa4d5d68f57c303af1bdade
Rafael Bozzetti <rafael.bozzetti@utech.com.br>
2019-02-26 09:00:09 -0300

    Ajuste contato e opengraph

--- email.php
+++ email.php
@@ -1,15 +1,27 @@
 <?php
 
-    ini_set( 'display_errors', 1 );
-    error_reporting( E_ALL );
-    
-    $from = (isset($_POST['email']) ? $_POST['email'] : "contato@jrsantosadvocacia.com.br");
+	$url = 'https://jrsantosadvocacia.000webhostapp.com';
+
+    $from = (isset($_POST['email']) ? $_POST['email'] : "site@jrsantosadvocacia.com.br");
     $to = 'joaorsantosadv@gmail.com';
 
+    $telefone = (isset($_POST['telefone']) ? $_POST['telefone'] : "Não Informado");
+    $celular = (isset($_POST['celular']) ? $_POST['celular'] : "Não Informado");
+
     $subject = "Contato pelo Site";
-    $message = $_POST['message'];
+    $message = ( isset($_POST['message']) ? $_POST['message'] : '');
+
+    $message = "
+    Contato recebido pelo Site: \n
+    Para: João Rodrigo dos Santos joaorsantosadv@gmail.com \n\n
+    De: {$_POST['name']} $from \n
+    Telefone: $telefone \n
+    Celular: $celular \n
+    
+    Mensagem: $message \n\n ";
+    
     $headers = "From:" . $from;
     mail($to,$subject,$message, $headers);
-    
-    header('/');   
+
+    header('Location: '. $url );   
 ?>
